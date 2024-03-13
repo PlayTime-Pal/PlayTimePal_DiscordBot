5046c26d0c7dce537666915e4ec7fb55e0045fca
David Sandrosyan <david-010803@mail.ru>
2024-03-13 20:35:13 +0300

    add json reader class in config

---
+++ PlayTimePal_Bot/config/JSONReader.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PlayTimePal_Bot.config
+{
+	internal class JSONReader
+	{
+		public string token {  get; set; }
+		public string prefix { get; set; }
+
+		public async Task ReadJSON()
+		{
+			using (StreamReader sr = new StreamReader("config.json"))
+			{
+				string json = await sr.ReadToEndAsync();
+				JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(json);
+
+				this.token = data.token;
+				this.prefix = data.prefix;
+			}
+		}
+	}
+
+	internal sealed class JSONStructure
+	{
+		public string token { get; set; }
+		public string prefix { get; set; }
+	}
+}
